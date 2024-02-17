d64ca8282d490308a568438f248fcffb0d09a66a
SoulBeast <WweveW@mail.ru>
2024-02-17 13:14:41 +0200

    Theme 21 RequireJS

---
+++ design/frontend/Perspective/Fronttheme/registration.php
@@ -0,0 +1,10 @@
+<?php
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::THEME,
+    'frontend/Perspective/Fronttheme',
+
+    __DIR__
+);
